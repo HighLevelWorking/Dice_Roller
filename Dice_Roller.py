06e43c47f34185478fc4f657c472cc4c6e84737b
HighLevelWorking <highlevelworking@gmail.com>
2025-06-25 18:30:46 +0500

    Update Dice_Roller.py
    Added a confirmation when exiting, A prompt to clear the screen, User can choose how many die to work with :)

--- Dice_Roller.py
+++ Dice_Roller.py
@@ -5,15 +5,16 @@ import random
 def Clear_Screen():
     os.system("cls")
 
+
+
 Roll_Choices = (1, 2, 3, 4, 5, 6)
 
 Validation = ("yes", "no")
 
-while True:
-    Screen = input("Do you want to clear the screen or no? (y/n): ").lower()
+Flag = True
 
-    if Screen == "y":
-        Clear_Screen()
+while True:
+    
 
     User_Choice = input("Do you want to roll the dice: (Yes/no): ").lower()
 
@@ -21,8 +22,25 @@ while True:
         User_Choice = input("Do you want to roll the dice: (yes/no): ").lower()
 
     if User_Choice == "yes":
-        Roll_Choice = random.choice(Roll_Choices)
-        print(Roll_Choice)
+        Num_Of_Die = int(input("How many 6 sided die do you want to use: "))
+        for i in range (0, Num_Of_Die):
+            Roll_Choice = random.choice(Roll_Choices)
+            print(f"{Roll_Choice: >2}", end = ", ")
     else: 
-        break
+        Exiting = input("Are you sure you want to exit this program? (yes/no) ").lower()
+        if Exiting == "yes":
+            break
+        
+    
+    print()
 
+    while Flag == True:
+        Screen = input("Do you want to clear the screen or no? (y/n) or (yes/no): ").lower()
+        if Screen == "y" or Screen == "yes":
+            Clear_Screen()
+            Flag = False
+        elif Screen == "n" or Screen =="no":
+            print("Your terminal wont be cleared", end = "\r")
+            Flag = False
+        else:
+            print("Please only enter yes/y or no/n as your input: ", end = "\r")    
