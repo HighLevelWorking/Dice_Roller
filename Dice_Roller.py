df16bc30d1194d2267f1cf4f429d668647bf3946
HighLevelWorking <highlevelworking@gmail.com>
2025-06-25 18:11:56 +0500

    Update Dice_Roller.py
    Works now yay!!!!!

--- Dice_Roller.py
+++ Dice_Roller.py
@@ -7,19 +7,19 @@ def Clear_Screen():
 
 Roll_Choices = (1, 2, 3, 4, 5, 6)
 
-
-
-Validation = ("yes", "No")
-
-
-
-
+Validation = ("yes", "no")
 
 while True:
-    User_Choice = input("Do you want to roll the dice: (Yes/no): ")
+    Screen = input("Do you want to clear the screen or no? (y/n): ").lower()
+
+    if Screen == "y":
+        Clear_Screen()
+
+    User_Choice = input("Do you want to roll the dice: (Yes/no): ").lower()
+
     while User_Choice not in Validation:
         User_Choice = input("Do you want to roll the dice: (yes/no): ").lower()
- 
+
     if User_Choice == "yes":
         Roll_Choice = random.choice(Roll_Choices)
         print(Roll_Choice)
